1164a0a854dd8b9f783477f5490a7375109b3972
La'Neice Lansey <LLansey@users.noreply.github.com>
2019-01-08 19:38:47 -0500

    adding data.mcafee file

---
+++ Module/Convert.Main/Data.McAfee.ps1
@@ -0,0 +1,36 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+<#
+    Instructions:  Use this file to add/update/delete regsitry expressions that are used accross 
+    multiple technologies files that are considered commonly used.  Enure expressions are listed
+    from MOST Restrive to LEAST Restrictive, similar to exception handling.  Also, ensure only
+    UNIQUE Keys are used in each hashtable to prevent errors and conflicts.  Within each table there
+    can be a single key for Contains, Match, and Select.  These keys match functions in the refactored
+    Functions.SingleLine.ps1 script in the RegistryRule module.  Example: See Data.Office.ps1
+#>
+$global:SingleLineRegistryPath += [ordered]@{
+    McAfee1 = [ordered]@{
+    Root = [ordered]@{
+        Match    = '(HKCU|HKLM|HKEY_LOCAL_MACHINE|HKEY_CURRENT_USER)\\'
+        Select   = '((HKLM|HKCU|HKEY_LOCAL_MACHINE|HKEY_CURRENT_USER).*(?=\s\(64-bit\)$)).*(\n.*$)'
+    }
+}
+
+$global:SingleLineRegistryValueName += [ordered]@{
+    One   = @{
+        Select = '(?<=If the value(\s*)?((for( )?)?)").*(")?((?=is.*R)|(?=does not exist))'
+    }
+}
+
+$global:SingleLineRegistryValueType += [ordered]@{
+    One   = @{
+        Select = '((HKLM|HKCU|HKEY_LOCAL_MACHINE|HKEY_CURRENT_USER).*(?=\s\(64-bit\)$)).*(\n.*$)'
+    }
+}
+
+$global:SingleLineRegistryValueData += [ordered]@{
+    One   = @{
+        Select = '(?<={0})(\s*)?=.*(?=(,|\())'
+    }
+}
